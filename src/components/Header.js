4f97d871ebac7981a4b9791b228d7768ac27ad87
nicolaslozada <57370618+nicolaslozada@users.noreply.github.com>
2023-07-05 22:04:02 -0500

    minor fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,10 @@ const abilities = ["Product Design", "User Research", "UI Design", "Brand Design
 export default function About() {
     return(
         <div className="red">
-            <h1 className="text-7xl">Hola! I’m Nico. I transform your ideas into exceptional digital products.</h1>
+            <h1 className="text-7xl">
+                Hola! I’m Nico. <br />
+                I transform your ideas into exceptional digital products.
+            </h1>
             <ul className="flex flex-wrap gap-2">
                 {abilities.map((ab) =>  
                     <li className="p-1 border rounded-full px-3">{ab}</li>
